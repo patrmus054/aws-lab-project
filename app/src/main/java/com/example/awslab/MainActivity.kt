52464a89a7598139a2e5cf58d33cb3b2401f7819
Patryk <musiol.patryk97@gmail.com>
2020-06-30 11:33:54 +0200

    test part 2

--- app/src/main/java/com/example/awslab/MainActivity.kt
+++ app/src/main/java/com/example/awslab/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "teest", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "AWS super jest", Toast.LENGTH_SHORT).show()
     }
 }
